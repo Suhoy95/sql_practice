3a5e52b83d7dd1af753d9f03a5663b23f89fc8fb
Suhoy95 <suhoy95@gmail.com>
2017-05-17 00:21:03 +0500

    first

---
+++ FILL.sql
@@ -0,0 +1,207 @@
+﻿-- справочник "Организации"
+    -- ORG_ID, ORG_S_NAME, OGR_F_NAME, ORG_INN, ORG_LEG_ADD, ORG_ACT_ADD, ORG_PRE
+INSERT INTO ORGANIZATIONS
+    (ORG_ID, ORG_S_NAME, OGR_F_NAME, ORG_INN, ORG_LEG_ADD, ORG_ACT_ADD, ORG_PRE) VALUES
+    (0, 'Мего код', 'ООО "МЕГА КОД"', '6669996660', 'Ижевск, Инд. р., ул. Л. Толстого, 11', 'Ижевск, Инд. р., ул. Л. Толстого, 11', 'SPC');
+INSERT INTO ORGANIZATIONS
+    (ORG_ID, ORG_S_NAME, OGR_F_NAME, ORG_INN, ORG_LEG_ADD, ORG_ACT_ADD, ORG_PRE) VALUES
+    (1, 'Ижтрейдинг', 'ООО "ИЖТРЕЙДИНГ"', '1833036726', '426039, г Ижевск, Воткинское шоссе,178а', '426039, г Ижевск, Воткинское шоссе,178а', 'IZT');
+INSERT INTO ORGANIZATIONS
+    (ORG_ID, ORG_S_NAME, OGR_F_NAME, ORG_INN, ORG_LEG_ADD, ORG_ACT_ADD, ORG_PRE) VALUES
+    (2, 'Вкусный дом', 'ООО "ВКУСНЫЙ ДОМ"', '1831133410', '426000, г Ижевск, Воткинское шоссе,17','426000, г Ижевск, Воткинское шоссе,17', 'VKD');
+INSERT INTO ORGANIZATIONS
+    (ORG_ID, ORG_S_NAME, OGR_F_NAME, ORG_INN, ORG_LEG_ADD, ORG_ACT_ADD, ORG_PRE) VALUES
+    (3, 'Пятерочка', 'ООО "ПЯТЕРОЧКА РЕГИОНЫ"', '7811339968', '192019, Г САНКТ-ПЕТЕРБУРГ, УЛ ГЛАЗУРНАЯ, 10 ЛИТЕР А','192019, Г САНКТ-ПЕТЕРБУРГ, УЛ ГЛАЗУРНАЯ, 10 ЛИТЕР А', 'PTR');
+INSERT INTO ORGANIZATIONS
+    (ORG_ID, ORG_S_NAME, OGR_F_NAME, ORG_INN, ORG_LEG_ADD, ORG_ACT_ADD, ORG_PRE) VALUES
+    (4, 'Магнит', 'ООО "МАГНИТ"', '1831086930', '426052, г Ижевск, ул Лесозаводская,23/164','Г ИЖЕВСК,УЛ 30 ЛЕТ ПОБЕДЫ,68', 'MGT');
+
+-- справочник "Подразделения"
+    -- SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID
+    -- + 1. Администрация
+    -- | - 5. Совет директоров
+    -- | - 6. Отдел топ менеджмента
+    -- + 2. Маркетинг
+    -- | - 7. Маркетологи
+    -- | - 8. Отдел продаж
+    -- + 3. Разработчики
+    -- | - 9. Проектировщики баз данных
+    -- | - 10. Разработчки ПО
+    -- - 4. Отдел тестирования
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (1, 'Администрация',                0, 'Управление',                         1, NULL);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (2, 'Маркетинг',                    0, 'Продажа услуг приложения',           1, NULL);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (3, 'Разработчики',                 0, 'Создание и поддержка приложений',    1, NULL);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (4, 'Отдел тестирования',           0, 'Контроль качества приложения',       1, NULL);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (5, 'Совет директоров',             0, 'Политика компании',                  1, 1);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (6, 'Отдел топ менеджмента',        0, 'Исполнители',                        1, 1);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (7, 'Маркетологи',                  0, 'Разработка и поддержка брэнда',    1, 2);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (8, 'Отдел продаж',                 0, 'Предоставление услуг клиентам',    1, 2);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (9, 'Проектировщики баз данных',    0, 'Управление базами данных',          1, 3);
+INSERT INTO SUBDIVISIONS (SUB_ID, SUB_NAME, ORG_ID, SUB_ROLE, SUB_REG, SUB_PARENT_ID) VALUES
+    (10, 'Разработчки ПО',              0, 'Разработка приложения',            1, 3);
+
+
+-- справочник "Должности"
+    -- POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID
+    --    + 1. Дизайнер
+    --    |     - 10. Дизайнер по персонажам
+    --    |     - 11. Дазайнер ландшафтов
+    --    + 2. Программист
+    --    |    - 12. Разработчик
+    --    |    - 13. Разработчик сюжета
+    --    |    - 14. Разработчик навыков
+    --    + 3. Маркетолог
+    --    |    - 15. Аналитик
+    --    |    - 16. SEO-специалист
+    --    |    - 17. SMM-специалист
+    --    |    - 18. Специалист по рекламе
+    --    - 4. Тестировщик
+    --    - 5. Системный администратор
+    --    - 6. Менеджер проекта
+    --    - 7. Менеджер по работе с персоналом
+    --    - 8. Бухгалтер
+    --    - 9. Директор
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (1, 0, 'Дизайнер',                            20000, 1, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (2, 0, 'Программист',                        20000, 1, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (3, 0, 'Маркетолог',                        18000, 1, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (4, 0, 'Тестировщик',                        25000, 0, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (5, 0, 'Системный администратор',            20000, 0, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (6, 0, 'Менеджер проекта',                    25800, 0, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (7, 0, 'Менеджер по работе с персоналом',    17600, 0, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (8, 0, 'Бухгалтер',                            21500, 0, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (9, 0, 'Директор',                            50000, 0, NULL);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (10,0, 'Дизайнер по персонажам',            22000, 0, 1);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (11,0, 'Дазайнер ландшафтов',                35000, 0, 1);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (12,0, 'Разработчик',                        20000, 0, 2);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (13,0, 'Разработчик сюжета',                32000, 0, 2);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (14,0, 'Разработчик навыков',                32000, 0, 2);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (15,0, 'Аналитик',                            25000, 0, 3);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (16,0, 'SEO-специалист',                    20000, 0, 3);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (17,0, 'SMM-специалист',                    20000, 0, 3);
+INSERT INTO POSITIONS (POS_ID, ORG_ID, POS_NAME, POS_SALARY, POS_IS_GROUP, POS_PARENT_ID) VALUES
+    (18,0, 'Специалист по рекламе',                18000, 0, 3);
+
+
+-- справочник "сотрудники"
+    -- EMP_ID, EMP_NAME, EMP_DATE, EMP_PASS, EMP_INN
+INSERT INTO EMPLOYEES VALUES (1,'Иванов Аркадий Акакиевич',TO_DATE('30.03.1991','DD.MM.YYYY'),
+'9410 1234 выдан отделом УФМС России по УР в Октябрьском р-не гор. Ижевска 17.02.2010',183454321234);
+INSERT INTO EMPLOYEES VALUES (2,'Абрикосов Алексей Алексеевич',TO_DATE('15.09.1975','DD.MM.YYYY'),
+'9415 9839 выдан отделом УФМС России по УР в Ленинском р-не гор. Ижевска 30.05.2005',185678769876);
+INSERT INTO EMPLOYEES VALUES (3,'Алферов Жорес Иванович',TO_DATE('18.10.1934','DD.MM.YYYY'),
+'9440 9843 выдан Индустриальным РОВД гор. Ижевска 15.12.2015',189064783652);
+INSERT INTO EMPLOYEES VALUES (4,'Никогдаев Марат Ашанович',TO_DATE('22.05.1976','DD.MM.YYYY'),
+'9432 8315 выдан Устиновским РОВД гор. Ижевска 16.07.2000',186479382035);
+INSERT INTO EMPLOYEES VALUES (5,'Миловашкин Никита Любовин',TO_DATE('26.08.1968','DD.MM.YYYY'),
+'9400 8314 выдан Индустриальным РОВД гор. Ижевска 15.09.2013',182345983765);
+INSERT INTO EMPLOYEES VALUES (6,'Мимишкина Анастасия Арсеньевна',TO_DATE('26.07.1980','DD.MM.YYYY'),
+'9430 4731 выдан отделом УФМС России по УР в Ленинском р-не гор. Ижевска 23.03.2010',182091850392);
+INSERT INTO EMPLOYEES VALUES (7,'Улюлюкина Мирана Дьяволовна',TO_DATE('07.01.1972','DD.MM.YYYY'),
+'9411 4542 выдан Индустриальным РОВД гор. Ижевска 23.01.1992',182345678765);
+INSERT INTO EMPLOYEES VALUES (8,'Ненавистина Мария Предметовна',NULL,
+'9413 3913 выдан Ленинским РОВД гор. Ижевска 31.01.2008',189098765432);
+
+
+-- справочник "Причины отсутствия"
+    -- ABS_ID, ABS_NAME, ABS_COEF, ABS_MAX_DAYS
+INSERT INTO ABSENCE (ABS_ID, ABS_NAME, ABS_COEF, ABS_MAX_DAYS) VALUES
+    (1,'Прогул',                0,    1);
+INSERT INTO ABSENCE (ABS_ID, ABS_NAME, ABS_COEF, ABS_MAX_DAYS) VALUES
+    (2,'Больничный',            1,    366);
+INSERT INTO ABSENCE (ABS_ID, ABS_NAME, ABS_COEF, ABS_MAX_DAYS) VALUES
+    (3,'Очередной отпуск',        1,    28);
+INSERT INTO ABSENCE (ABS_ID, ABS_NAME, ABS_COEF, ABS_MAX_DAYS) VALUES
+    (4,'Отпуск за свой счёт',    0,    56);
+
+-- справочник "Производственный календарь"
+    -- TIME_ID, TIME_DATE, TIME_HOUR;
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (1,TO_DATE('21.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (2,TO_DATE('22.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (3,TO_DATE('23.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (4,TO_DATE('26.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (5,TO_DATE('27.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (6,TO_DATE('28.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (7,TO_DATE('29.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (8,TO_DATE('30.08.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (9,TO_DATE('03.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (10,TO_DATE('04.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (11,TO_DATE('05.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (12,TO_DATE('06.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (13,TO_DATE('07.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (14,TO_DATE('10.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (15,TO_DATE('11.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (16,TO_DATE('12.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (17,TO_DATE('13.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (18,TO_DATE('14.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (19,TO_DATE('17.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (20,TO_DATE('18.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (21,TO_DATE('19.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (22,TO_DATE('20.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (23,TO_DATE('21.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (24,TO_DATE('24.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (25,TO_DATE('25.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (26,TO_DATE('26.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (27,TO_DATE('27.09.2016','DD.MM.YYYY'),8);
+INSERT INTO TIMETABLE (TIME_ID, TIME_DATE, TIME_HOUR) VALUES (28,TO_DATE('28.09.2016','DD.MM.YYYY'),8);
+
+
+-- Периодический справочник "Должности сотрудников"
+    -- PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.09.2016','DD.MM.YYYY'), 4, 1, 18, 1, 50000, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.09.2016','DD.MM.YYYY'), 5, 4, 4,        1, 20000, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.09.2016','DD.MM.YYYY'), 4, 5, 17,    1, 21500, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.09.2016','DD.MM.YYYY'), 6, 3, 7,        1, 32000, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.09.2016','DD.MM.YYYY'), 4, 7, 16,    1, 17600, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.09.2016','DD.MM.YYYY'), 3, 6, 12,    1, 18000, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.10.2016','DD.MM.YYYY'), 5, 4, 4,        1, 20000, 0);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.10.2016','DD.MM.YYYY'), 5, 2, 4,        1, 20000, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('21.10.2016','DD.MM.YYYY'), 6, 4, 6,        1, 35000, 1);
+INSERT INTO POSITIONS_EMPLOYEES
+    (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID, PE_RATE, PE_SALARY, PE_STATE) VALUES 
+    (TO_DATE('24.10.2016','DD.MM.YYYY'), 1, 8, 15,    1, 25800, 1);
+
+
---
+++ INSTALL.sql
@@ -0,0 +1,231 @@
+﻿-- #############################################################################
+-- ##                                                                         ##
+-- ## СПРАВОЧНИКИ                                                             ##
+-- ##                                                                         ##
+-- #############################################################################
+
+-- справочник "Организации"
+CREATE TABLE ORGANIZATIONS (
+-- fields
+    ORG_ID NUMBER NOT NULL,
+    ORG_S_NAME VARCHAR2(20CHAR) NOT NULL, -- short(кор.) название
+    OGR_F_NAME VARCHAR2(100CHAR) NOT NULL, -- full(полн.) название
+    ORG_INN VARCHAR2(12CHAR) NOT NULL, -- ИНН
+    ORG_LEG_ADD VARCHAR2(200CHAR) NOT NULL, -- legal(юр.) адрес
+    ORG_ACT_ADD VARCHAR2(200CHAR) NOT NULL, -- actual(факт.) фдрес
+    ORG_PRE VARCHAR2(3CHAR) NOT NULL, -- префикс рорганизации для документов
+-- constraints
+    CONSTRAINT PK_ORG_ID PRIMARY KEY (ORG_ID),
+    CONSTRAINT ORG_PRE_U UNIQUE (ORG_PRE)
+);
+
+-- справочник "Подразделения"
+CREATE TABLE SUBDIVISIONS (
+-- fields
+    SUB_ID NUMBER NOT NULL,
+    SUB_NAME VARCHAR2(100CHAR) NOT NULL, -- Название подразделения
+    ORG_ID NUMBER NOT NULL, -- Ссылка на справочник "Организации"
+    SUB_ROLE VARCHAR2(100CHAR), -- Роль подразделения.
+    SUB_REG NUMBER NOT NULL, -- Региональная надбавка (коэффициент, учитываемый при расчете зарплаты).
+    SUB_PARENT_ID NUMBER, -- Ссылка на родителя.
+-- constraints
+    CONSTRAINT PK_SUB_ID PRIMARY KEY (SUB_ID),
+    CONSTRAINT FK_SUB_PARENT FOREIGN KEY (SUB_PARENT_ID) REFERENCES SUBDIVISIONS (SUB_ID),
+    CONSTRAINT FK_SUB_to_ORG FOREIGN KEY (ORG_ID) REFERENCES ORGANIZATIONS (ORG_ID)
+);
+
+-- справочник "Должности"
+CREATE TABLE POSITIONS (
+-- fields
+    POS_ID NUMBER NOT NULL,
+    ORG_ID NUMBER NOT NULL, -- Ссылка на справочник "Организации"
+    POS_NAME VARCHAR2(100CHAR) NOT NULL, -- Название должности.
+    POS_SALARY NUMBER, -- Оклад - используется как значение по умолчанию при заполнении формы документа.
+    POS_IS_GROUP NUMBER NOT NULL, -- 0 – элемент / 1 – группа
+    POS_PARENT_ID NUMBER, -- Ссылка на родителя.
+-- constraints
+    CONSTRAINT PK_POS_ID PRIMARY KEY (POS_ID),
+    CONSTRAINT FK_POS_PARENT FOREIGN KEY (POS_PARENT_ID) REFERENCES POSITIONS (POS_ID),
+    CONSTRAINT FK_POS_to_ORG FOREIGN KEY (ORG_ID) REFERENCES ORGANIZATIONS (ORG_ID),
+    CONSTRAINT IS_GROUP CHECK(POS_IS_GROUP IN (0,1))
+);
+
+-- справочник "сотрудники"
+CREATE TABLE EMPLOYEES (
+-- fields
+    EMP_ID NUMBER NOT NULL,
+    EMP_NAME VARCHAR2(100CHAR) NOT NULL, -- ФИО
+    EMP_DATE DATE, -- Дата рождения.
+    EMP_PASS VARCHAR2(300CHAR) NOT NULL, -- Паспортные данные.
+    EMP_INN VARCHAR2(12CHAR), -- ИНН (NULL - ИНН отсутствует).
+-- constraints
+    CONSTRAINT PK_EMP_ID PRIMARY KEY (EMP_ID)
+);
+
+-- справочник "Причины отсутствия"
+CREATE TABLE ABSENCE (
+-- fields
+    ABS_ID NUMBER NOT NULL,
+    ABS_NAME VARCHAR2(255CHAR) NOT NULL, -- Название причины отсутствия (Прогул/больничный/очередной отпуск/отпуск за свой счет)
+    ABS_COEF NUMBER NOT NULL, -- Коэффициент ставки
+    ABS_MAX_DAYS NUMBER NOT NULL, -- Ограничение по количеству дней в году
+-- constraints
+    CONSTRAINT PK_ABS_ID PRIMARY KEY (ABS_ID)
+);
+
+-- справочник "Производственный календарь"
+CREATE TABLE TIMETABLE (
+-- fields
+    TIME_ID NUMBER NOT NULL,
+    TIME_DATE DATE NOT NULL, -- Дата 
+    TIME_HOUR NUMBER DEFAULT 8 NOT NULL, -- Количество рабочих часов
+-- constraints
+    CONSTRAINT PK_TIME_ID PRIMARY KEY (TIME_ID),
+    CONSTRAINT UNIQUE_DATE UNIQUE (TIME_DATE)
+);
+
+-- Периодический справочник "Должности сотрудников"
+CREATE TABLE POSITIONS_EMPLOYEES (
+-- fields
+    PE_VALID_SINCE DATE NOT NULL, -- Дата изменения
+    SUB_ID NUMBER NOT NULL, -- Ссылка на справочник «Подразделения».
+    EMP_ID NUMBER NOT NULL, -- Ссылка на справочник «Сотрудники»
+    POS_ID NUMBER NOT NULL, -- Ссылка на справочник «Должности».
+    PE_RATE NUMBER NOT NULL, -- Ставка (относительно нормативной).
+    PE_SALARY NUMBER NOT NULL, -- Оклад (рублей для полной ставки).
+    PE_STATE NUMBER NOT NULL, -- Статус 0 – уволен / 1 – принят
+-- constraints
+    CONSTRAINT PK_PE_ID PRIMARY KEY (PE_VALID_SINCE, SUB_ID, EMP_ID, POS_ID),
+    CONSTRAINT FK_PE_to_SUB FOREIGN KEY (SUB_ID) REFERENCES SUBDIVISIONS (SUB_ID),
+    CONSTRAINT FK_PE_to_EMP FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEES (EMP_ID),
+    CONSTRAINT FK_PE_to_POS FOREIGN KEY (POS_ID) REFERENCES POSITIONS (POS_ID),
+    CONSTRAINT PE_RATE_VALUE CHECK (PE_RATE > 0 AND PE_RATE <= 1),
+    CONSTRAINT PE_SALARY_VALUE CHECK (PE_SALARY > 0),
+    CONSTRAINT PE_WORK_STATE CHECK (PE_STATE IN (0,1))
+);
+
+-- #############################################################################
+-- ##                                                                         ##
+-- ## ДОкументы                                                               ##
+-- ##                                                                         ##
+-- #############################################################################
+
+-- документ "Кадровый приказ", шапка
+CREATE TABLE STAFF_ORDER_HEADER (
+-- fields
+    SO_ID NUMBER NOT NULL,
+    SO_DATE DATE NOT NULL, -- Дата создания.
+    SO_NUMBER VARCHAR2(30CHAR) NOT NULL, -- Номер документа.
+    ORG_ID NUMBER NOT NULL, -- Ссылка на справочник "Организации"
+    SO_STATE NUMBER DEFAULT 0 NOT NULL, -- Статус документа 0 – черновой / 1 – подтверждённый
+-- constraints
+    CONSTRAINT PK_SO_ID PRIMARY KEY (SO_ID),
+    CONSTRAINT FK_SO_to_ORG FOREIGN KEY (ORG_ID) REFERENCES ORGANIZATIONS (ORG_ID),
+    CONSTRAINT SOT_STATE CHECK (SO_STATE IN (0, 1))
+);
+
+-- документ "Кадровый приказ", табличная часть
+CREATE TABLE STAFF_ORDER_LINES (
+-- fields
+    SOL_ID NUMBER NOT NULL,
+    SO_ID NUMBER NOT NULL, -- Ссылка на заголовок документа.
+    EMP_ID NUMBER NOT NULL, -- Ссылка на справочник «Сотрудники».
+    SUB_ID NUMBER NOT NULL, -- Ссылка на справочник «Подразделения».
+    POS_ID NUMBER NOT NULL, -- Ссылка на справочник «Должности».
+    SOL_TYPE NUMBER NOT NULL, -- 0 – увольнение, 1 – приём, 2 – изменение
+    SOL_RATE NUMBER, -- Ставка (> 0 & <= 1)
+    SOL_SALARY NUMBER, -- Оклад (>0)
+-- constraints
+    CONSTRAINT PK_SOL_ID PRIMARY KEY (SOL_ID),
+    CONSTRAINT FK_SOL_to_SO FOREIGN KEY (SO_ID) REFERENCES STAFF_ORDER_HEADER (SO_ID),
+    CONSTRAINT FK_SOL_to_EMP FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEES (EMP_ID),
+    CONSTRAINT FK_SOL_to_SUB FOREIGN KEY (SUB_ID) REFERENCES SUBDIVISIONS (SUB_ID),
+    CONSTRAINT FK_SOL_to_POS FOREIGN KEY (POS_ID) REFERENCES POSITIONS (POS_ID),
+    CONSTRAINT SOL_TYPE_VALUE CHECK (SOL_TYPE IN (0,1,2)),
+    CONSTRAINT SOL_RATE_VALUE CHECK (SOL_RATE > 0 AND SOL_RATE <= 1),
+    CONSTRAINT SOL_SALARY_VALUE CHECK (SOL_SALARY > 0)
+);
+
+-- документ "отсутствие сотрудника"
+CREATE TABLE ABSENCE_EMPLOYEE (
+-- fields
+    AE_ID NUMBER NOT NULL,
+    AE_DATE DATE NOT NULL, -- Дата события
+    AE_NUMBER VARCHAR2(30CHAR) NOT NULL, -- Номер документа
+    AE_STATE NUMBER DEFAULT 0 NOT NULL, -- Статус документа 0 - черновой, 1 - подтвержденный
+    SUB_ID NUMBER NOT NULL, -- Ссылка на справочник  «Подразделения»
+    EMP_ID NUMBER NOT NULL, -- Ссылка на справочник  «Сотрудник»
+    ABS_ID NUMBER NOT NULL, -- Ссылка на справочник "Причина отсутствия" 
+    AE_START DATE NOT NULL, -- Начало отсутствия
+    AE_END DATE NOT NULL, -- Окончание отсутствия 
+-- constraints
+    CONSTRAINT PK_AE_ID PRIMARY KEY (AE_ID),
+    CONSTRAINT FK_AE_to_SUB FOREIGN KEY (SUB_ID) REFERENCES SUBDIVISIONS (SUB_ID),
+    CONSTRAINT FK_AE_to_EMP FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEES (EMP_ID),
+    CONSTRAINT FK_AE_to_ABS FOREIGN KEY (ABS_ID) REFERENCES ABSENCE (ABS_ID),
+    CONSTRAINT ABS_STATE CHECK (AE_STATE IN (0,1))
+);
+
+-- документ "начисление зарплаты", шапка
+CREATE TABLE CALCULATION_SALARY_HEADER (
+-- fields
+    CS_ID NUMBER NOT NULL,
+    CS_DATE DATE NOT NULL, -- Дата события
+    CS_NUMBER VARCHAR2(30CHAR) NOT NULL, -- Номер документа
+    CS_STATE NUMBER NOT NULL, -- Статус документа  0 - черновой, 1 - подтвержденный
+    SUB_ID NUMBER NOT NULL, -- Ссылка на справочник  «Подразделения»
+-- constraints
+    CONSTRAINT PK_CS_ID PRIMARY KEY (CS_ID),
+    CONSTRAINT FK_CS_to_SUB FOREIGN KEY (SUB_ID) REFERENCES SUBDIVISIONS (SUB_ID),
+    CONSTRAINT CST_STATE CHECK (CS_STATE IN (0,1))
+);
+
+-- документ "начисление зарплаты", табличная часть
+CREATE TABLE CALCULATION_SALARY_LINES (
+-- fields
+    CSL_ID NUMBER NOT NULL,
+    CS_ID NUMBER NOT NULL, -- Ссылка на шапку документа 
+    EMP_ID NUMBER NOT NULL, -- Ссылка на справочник  «Сотрудник»
+    CSL_TIME NUMBER NOT NULL, -- Количество часов (фактическое) 
+    CSL_CONSIDER NUMBER NOT NULL, -- Сумма зарплаты   расчетная 
+    CSL_PRIZE NUMBER NOT NULL, -- Премия
+    CSL_CORRECTION NUMBER NOT NULL, -- Сумма зарплаты скорректированная
+-- constraints
+    CONSTRAINT PK_CSL_ID PRIMARY KEY (CSL_ID),
+    CONSTRAINT FK_CSL_to_CS FOREIGN KEY (CS_ID) REFERENCES CALCULATION_SALARY_HEADER (CS_ID),
+    CONSTRAINT FK_CSL_to_EMP FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEES (EMP_ID)
+);
+
+-- #############################################################################
+-- ##                                                                         ##
+-- ## Кубы                                                                    ##
+-- ##                                                                         ##
+-- #############################################################################
+
+-- куб "Взаиморасчёты с сотрудниками"
+CREATE TABLE CALCULATION_WITH_EMPLOYEES (
+-- fields
+    CSL_ID NUMBER NOT NULL, -- Ссылка на запись в документе "Начисление зарплат"
+    EMP_ID NUMBER NOT NULL, -- Ссылка на справочник «Сотрудники».
+    CWE_TYPE VARCHAR2(15CHAR) NOT NULL, -- Тип выплаты (простая, целевые расходы).
+    CWE_SUM NUMBER NOT NULL, -- Сумма.
+-- constraints
+    CONSTRAINT PK_CWE_ID PRIMARY KEY (CSL_ID, EMP_ID),
+    CONSTRAINT FK_CWE_to_CSL FOREIGN KEY (CSL_ID) REFERENCES CALCULATION_SALARY_LINES (CSL_ID),
+    CONSTRAINT FK_CWE_to_EMP FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEES (EMP_ID)
+);
+
+-- куб "Отклонения расчёта"
+CREATE TABLE DEVATION_CALCULATION (
+-- fields
+    AE_ID NUMBER NOT NULL, -- Ссылка на документ "Отсутствие сотрудника"
+    EMP_ID NUMBER NOT NULL, -- Ссылка на справочник  «Сотрудник»
+    DC_BEGIN DATE NOT NULL, -- Начало периода
+    DC_END DATE NOT NULL, -- Окончание периода
+    DC_TYPE VARCHAR2(20CHAR) NOT NULL, -- Тип отклонения (коэффициент / сумма)
+    DC_VALUE NUMBER NOT NULL, -- Отклонение
+-- constraints
+    CONSTRAINT PK_DC_ID PRIMARY KEY (AE_ID, EMP_ID, DC_BEGIN, DC_END),
+    CONSTRAINT FK_DC_to_AE FOREIGN KEY (AE_ID) REFERENCES ABSENCE_EMPLOYEE (AE_ID),
+    CONSTRAINT FK_DC_to_EMP FOREIGN KEY (EMP_ID) REFERENCES EMPLOYEES (EMP_ID)
+);
